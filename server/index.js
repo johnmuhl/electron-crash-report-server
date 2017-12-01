50e766dd1cfeaadf04e253461431c75eb25d1260
john muhl <git@johnmuhl.com>
2017-12-01 15:00:47 -0600

    fix heroku error

--- server/index.js
+++ server/index.js
@@ -17,270 +17,270 @@ const UNDEFINED_TABLE = '42P01'
 dotenv.config()
 
 const server = new Hapi.Server({
-	port: process.env.PORT,
-	router: {stripTrailingSlash: true},
+  port: process.env.PORT,
+  router: {stripTrailingSlash: true},
 })
 
 async function main () {
-	let db
-	let dumps
+  let db
+  let dumps
 
-	try {
-		db = await massive(process.env.DATABASE_URL)
-	} catch (error) {
-		throw new Error(error)
-	}
+  try {
+    db = await massive(process.env.DATABASE_URL)
+  } catch (error) {
+    throw new Error(error)
+  }
 
-	try {
-		dumps = await db.run('SELECT * FROM dumps')
-	} catch (error) {
-		if (error.code !== UNDEFINED_TABLE) throw new Error(error)
-	}
+  try {
+    dumps = await db.run('SELECT * FROM dumps')
+  } catch (error) {
+    if (error.code !== UNDEFINED_TABLE) throw new Error(error)
+  }
 
-	if (dumps) {
-		try {
-			// Add new columns to reports
-			await db.run('ALTER TABLE reports ADD COLUMN dump bytea')
-			await db.run('ALTER TABLE reports ADD COLUMN open boolean DEFAULT TRUE')
-			await db.run('ALTER TABLE reports ADD COLUMN closed_at timestamptz')
-			await db.run(
-				'ALTER TABLE reports ADD COLUMN updated_at timestamptz DEFAULT now()'
-			)
-			// Migrate dumps table to reports.dump
-			dumps.forEach(async dump => {
-				await db.reports.update({
-					id: dump.report_id,
-					dump: dump.file,
-				})
-			})
-			// Add NOT NULL to dump column
-			await db.run('ALTER TABLE reports ALTER COLUMN dump SET NOT NULL')
-			// Drop old dumps table
-			await db.run('DROP TABLE dumps')
-		} catch (error) {
-			throw new Error(error)
-		}
-	}
+  if (dumps) {
+    try {
+      // Add new columns to reports
+      await db.run('ALTER TABLE reports ADD COLUMN dump bytea')
+      await db.run('ALTER TABLE reports ADD COLUMN open boolean DEFAULT TRUE')
+      await db.run('ALTER TABLE reports ADD COLUMN closed_at timestamptz')
+      await db.run(
+        'ALTER TABLE reports ADD COLUMN updated_at timestamptz DEFAULT now()'
+      )
+      // Migrate dumps table to reports.dump
+      dumps.forEach(async dump => {
+        await db.reports.update({
+          id: dump.report_id,
+          dump: dump.file,
+        })
+      })
+      // Add NOT NULL to dump column
+      await db.run('ALTER TABLE reports ALTER COLUMN dump SET NOT NULL')
+      // Drop old dumps table
+      await db.run('DROP TABLE dumps')
+    } catch (error) {
+      console.error(error)
+    }
+  }
 
-	try {
-		await server.register([Basic, Inert, Vision])
-	} catch (error) {
-		throw new Error(error)
-	}
+  try {
+    await server.register([Basic, Inert, Vision])
+  } catch (error) {
+    throw new Error(error)
+  }
 
-	try {
-		// Prepare database
-		await db.run(SQL)
-	} catch (error) {
-		throw new Error(error)
-	}
+  try {
+    // Prepare database
+    await db.run(SQL)
+  } catch (error) {
+    throw new Error(error)
+  }
 
-	// Load, configure plugins
-	server.auth.strategy('simple', 'basic', {validate})
-	server.views({
-		engines: {html: Handlebars},
-		relativeTo: __dirname,
-		layout: true,
-		path: 'views',
-	})
+  // Load, configure plugins
+  server.auth.strategy('simple', 'basic', {validate})
+  server.views({
+    engines: {html: Handlebars},
+    relativeTo: __dirname,
+    layout: true,
+    path: 'views',
+  })
 
-	// Application state
-	server.app.db = db
-	server.app.env = process.env.NODE_ENV
-	server.app.user = process.env.AUTH_USER
-	server.app.pass = process.env.AUTH_PASS
-	server.app.auth = `${server.app.user}:${server.app.pass}`
+  // Application state
+  server.app.db = db
+  server.app.env = process.env.NODE_ENV
+  server.app.user = process.env.AUTH_USER
+  server.app.pass = process.env.AUTH_PASS
+  server.app.auth = `${server.app.user}:${server.app.pass}`
 
-	// Application routes
-	// route: GET /
-	server.route({
-		method: 'GET',
-		path: '/',
-		options: {
-			auth: 'simple',
-			handler: (request, h) => {
-				const auth = Buffer.from(server.app.auth).toString('base64')
-				const isSecure = server.app.env === 'production'
-				const options = {isHttpOnly: false, isSecure}
+  // Application routes
+  // route: GET /
+  server.route({
+    method: 'GET',
+    path: '/',
+    options: {
+      auth: 'simple',
+      handler: (request, h) => {
+        const auth = Buffer.from(server.app.auth).toString('base64')
+        const isSecure = server.app.env === 'production'
+        const options = {isHttpOnly: false, isSecure}
 
-				return h.view('index').state('authorization', auth, options)
-			},
-		},
-	})
+        return h.view('index').state('authorization', auth, options)
+      },
+    },
+  })
 
-	// route: POST /
-	server.route({
-		method: 'POST',
-		path: '/',
-		options: {
-			handler: async (request, h) => {
-				if (request.payload) {
-					const body = Object.assign({}, request.payload)
-					const dump = request.payload.upload_file_minidump
+  // route: POST /
+  server.route({
+    method: 'POST',
+    path: '/',
+    options: {
+      handler: async (request, h) => {
+        if (request.payload) {
+          const body = Object.assign({}, request.payload)
+          const dump = request.payload.upload_file_minidump
 
-					delete body.upload_file_minidump
+          delete body.upload_file_minidump
 
-					try {
-						await server.app.db.reports.save({body, dump})
+          try {
+            await server.app.db.reports.save({body, dump})
 
-						return {}
-					} catch (error) {
-						throw new Error(error)
-					}
-				} else {
-					return Boom.badRequest()
-				}
-			},
-		},
-	})
+            return {}
+          } catch (error) {
+            throw new Error(error)
+          }
+        } else {
+          return Boom.badRequest()
+        }
+      },
+    },
+  })
 
-	// route: GET /reports
-	server.route({
-		method: 'GET',
-		path: '/reports',
-		options: {
-			auth: 'simple',
-			handler: async (request, h) => {
-				const sql = 'SELECT * FROM reports ORDER BY created_at DESC'
+  // route: GET /reports
+  server.route({
+    method: 'GET',
+    path: '/reports',
+    options: {
+      auth: 'simple',
+      handler: async (request, h) => {
+        const sql = 'SELECT * FROM reports ORDER BY created_at DESC'
 
-				try {
-					const reports = await server.app.db.run(sql)
+        try {
+          const reports = await server.app.db.run(sql)
 
-					return reports.map(r => {
-						const report = Object.assign({}, r)
+          return reports.map(r => {
+            const report = Object.assign({}, r)
 
-						delete report.dump
-						delete report.search
+            delete report.dump
+            delete report.search
 
-						return report
-					})
-				} catch (error) {
-					throw new Error(error)
-				}
-			},
-		},
-	})
+            return report
+          })
+        } catch (error) {
+          throw new Error(error)
+        }
+      },
+    },
+  })
 
-	// route: GET /reports/:id
-	server.route({
-		method: 'GET',
-		path: '/reports/{id}',
-		options: {
-			auth: 'simple',
-			handler: async (request, h) => {
-				const id = Number(request.params.id)
+  // route: GET /reports/:id
+  server.route({
+    method: 'GET',
+    path: '/reports/{id}',
+    options: {
+      auth: 'simple',
+      handler: async (request, h) => {
+        const id = Number(request.params.id)
 
-				try {
-					const report = await server.app.db.reports.find(id)
+        try {
+          const report = await server.app.db.reports.find(id)
 
-					delete report.dump
-					delete report.search
+          delete report.dump
+          delete report.search
 
-					return report
-				} catch (error) {
-					throw new Error(error)
-				}
-			},
-		},
-	})
+          return report
+        } catch (error) {
+          throw new Error(error)
+        }
+      },
+    },
+  })
 
-	// route: PATCH /reports/:id
-	server.route({
-		method: 'PATCH',
-		path: '/reports/{id}',
-		options: {
-			auth: 'simple',
-			handler: async (request, h) => {
-				const id = Number(request.params.id)
+  // route: PATCH /reports/:id
+  server.route({
+    method: 'PATCH',
+    path: '/reports/{id}',
+    options: {
+      auth: 'simple',
+      handler: async (request, h) => {
+        const id = Number(request.params.id)
 
-				try {
-					const report = await server.app.db.reports.find(id)
-					const closedAt = report.open ? new Date() : null
+        try {
+          const report = await server.app.db.reports.find(id)
+          const closedAt = report.open ? new Date() : null
 
-					report.open = !report.open
-					report.closed_at = closedAt
+          report.open = !report.open
+          report.closed_at = closedAt
 
-					return await server.app.db.reports.save(report)
-				} catch (error) {
-					throw new Error(error)
-				}
-			},
-		},
-	})
+          return await server.app.db.reports.save(report)
+        } catch (error) {
+          throw new Error(error)
+        }
+      },
+    },
+  })
 
-	// route: DELETE /reports/:id
-	server.route({
-		method: 'DELETE',
-		path: '/reports/{id}',
-		options: {
-			auth: 'simple',
-			handler: async (request, h) => {
-				const id = Number(request.params.id)
+  // route: DELETE /reports/:id
+  server.route({
+    method: 'DELETE',
+    path: '/reports/{id}',
+    options: {
+      auth: 'simple',
+      handler: async (request, h) => {
+        const id = Number(request.params.id)
 
-				try {
-					return await server.app.db.reports.destroy(id)
-				} catch (error) {
-					throw new Error(error)
-				}
-			},
-		},
-	})
+        try {
+          return await server.app.db.reports.destroy(id)
+        } catch (error) {
+          throw new Error(error)
+        }
+      },
+    },
+  })
 
-	// route: GET /reports/:id/dump
-	server.route({
-		method: 'GET',
-		path: '/reports/{id}/dump',
-		options: {
-			auth: 'simple',
-			handler: async (request, h) => {
-				const id = Number(request.params.id)
-				const name = `crash-${id}.dmp`
+  // route: GET /reports/:id/dump
+  server.route({
+    method: 'GET',
+    path: '/reports/{id}/dump',
+    options: {
+      auth: 'simple',
+      handler: async (request, h) => {
+        const id = Number(request.params.id)
+        const name = `crash-${id}.dmp`
 
-				try {
-					const report = await server.app.db.reports.find(id)
+        try {
+          const report = await server.app.db.reports.find(id)
 
-					return h
-						.response(report.dump)
-						.header('content-disposition', `attachment; filename=${name}`)
-						.type('application/x-dmp')
-				} catch (error) {
-					throw new Error(error)
-				}
-			},
-		},
-	})
+          return h
+            .response(report.dump)
+            .header('content-disposition', `attachment; filename=${name}`)
+            .type('application/x-dmp')
+        } catch (error) {
+          throw new Error(error)
+        }
+      },
+    },
+  })
 
-	// Server client side javascript
-	server.route({
-		method: 'GET',
-		path: '/client/{path}',
-		options: {
-			handler: (request, h) => {
-				return h.file(resolve('client', request.params.path))
-			},
-		},
-	})
+  // Server client side javascript
+  server.route({
+    method: 'GET',
+    path: '/client/{path}',
+    options: {
+      handler: (request, h) => {
+        return h.file(resolve('client', request.params.path))
+      },
+    },
+  })
 
-	// Serve static assets
-	server.route({
-		method: 'GET',
-		path: '/{path}',
-		options: {
-			handler: (request, h) =>
-				h.file(resolve(__dirname, 'public', request.params.path)),
-		},
-	})
+  // Serve static assets
+  server.route({
+    method: 'GET',
+    path: '/{path}',
+    options: {
+      handler: (request, h) =>
+        h.file(resolve(__dirname, 'public', request.params.path)),
+    },
+  })
 
-	// Start server
-	try {
-		await server.start()
-	} catch (error) {
-		throw new Error(error)
-	}
+  // Start server
+  try {
+    await server.start()
+  } catch (error) {
+    throw new Error(error)
+  }
 
-	console.log(`Server running at: ${server.info.uri}`)
+  console.log(`Server running at: ${server.info.uri}`)
 
-	return server
+  return server
 }
 
 main()
