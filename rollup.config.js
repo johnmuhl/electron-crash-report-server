ec1b5975b4008c63c638a4b460ee3b1b1bc6f3cf
john muhl <git@johnmuhl.com>
2018-02-04 16:33:11 -0600

    preprocess styles w/ cssnext

--- rollup.config.js
+++ rollup.config.js
@@ -1,9 +1,9 @@
 import cjs from "rollup-plugin-commonjs";
-// import cssnext from "postcss-cssnext";
+import cssnext from "postcss-cssnext";
 import img from "rollup-plugin-img";
 import json from "rollup-plugin-json";
 import minify from "rollup-plugin-babel-minify";
-// import postcss from "rollup-plugin-postcss";
+import postcss from "postcss";
 import resolve from "rollup-plugin-node-resolve";
 import svelte from "rollup-plugin-svelte";
 
@@ -19,14 +19,17 @@ export default {
     cjs(),
     json(),
     resolve(),
-    // postcss({
-    //   extract: "server/public/bundle.css",
-    //   minimize: { autoprefixer: false },
-    //   plugins: [cssnext],
-    //   sourceMap: true,
-    // }),
     svelte({
       css: css => css.write("server/public/bundle.css"),
+      preprocess: {
+        style: async ({ content }) => {
+          const { css } = await postcss([cssnext]).process(content, {
+            from: undefined,
+          });
+
+          return { code: css };
+        },
+      },
       store: true,
     }),
     img(),
