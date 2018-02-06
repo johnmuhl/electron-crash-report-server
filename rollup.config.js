7fc10b75fa1f40197964a971327a6e4f5615c2b0
john muhl <git@johnmuhl.com>
2018-02-06 12:17:48 -0600

    🎨

--- rollup.config.js
+++ rollup.config.js
@@ -7,6 +7,8 @@ import postcss from "postcss";
 import resolve from "rollup-plugin-node-resolve";
 import svelte from "rollup-plugin-svelte";
 
+const production = process.env.NODE_ENV === 'production';
+
 export default {
   input: "client/index.js",
   output: {
@@ -20,7 +22,9 @@ export default {
     json(),
     resolve(),
     svelte({
+      cascade: false,
       css: css => css.write("server/public/bundle.css"),
+      dev: !production,
       preprocess: {
         style: async ({ content }) => {
           const { css } = await postcss([cssnext]).process(content, {
